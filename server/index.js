24d92ca1a6a9603f956f7d2011df444ba7a4e727
Francesca <ringot.france@gmail.com>
2018-03-31 13:31:04 +0200

    import mocks

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const mem1 = require('../mock/member/member1.json')
+const mem2 = require('../mock/member/member2.json')
+
+const mem = [ mem1, mem2]
+console.log(mem)
 
 const app = express()
 

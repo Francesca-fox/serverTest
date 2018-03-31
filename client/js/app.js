15fea53caa1037b35f72002f37b33b059cd00c2d
Francesca <ringot.france@gmail.com>
2018-03-31 15:31:14 +0200

    chgt sur app.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,14 @@
-console.log("je suis bien chargé")
+fetch('http://localhost:3240/mem')
+	.then(respose => response.json())
+	.then (mem => {
+
+		console.log(mem)
+	})
+
+// on veut luire dire qu'il aille sur l'autre canal 3240
+// car sinon celui du serve client est sur 5000
+// donc 2 sites diff
+// quand je fais un fetch , on lui passe une callback qui 
+// une callback, plusieurs eth dans la resposne pour comment
+// recup ce fichier
+// seconde .then = ma donnée que je vais afficher
